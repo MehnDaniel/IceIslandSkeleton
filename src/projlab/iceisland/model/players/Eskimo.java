c6879d43a5d435e9e81e722f46139c85a55b37cc
Szacsi5104 <61117705+Szacsi5104@users.noreply.github.com>
2020-03-28 23:02:27 +0100

    2 tolas

--- src/projlab/iceisland/model/players/Eskimo.java
+++ src/projlab/iceisland/model/players/Eskimo.java
@@ -5,6 +5,7 @@ import projlab.iceisland.model.Player;
 public class Eskimo extends Player {
     public Eskimo(Thing startingItem) {
         super(5, 4, startingItem);
+
     }
     @Override
     public void buildIgloo() {
