7912bbc531910bef6f8f6654e6f398b0f75e4f1f
murabrigi <46762705+murabrigi@users.noreply.github.com>
2020-03-29 14:00:30 +0200

    thingusagestrategies called, returned hívások

--- src/projlab/iceisland/model/thingusagestrategies/BarehandsDig.java
+++ src/projlab/iceisland/model/thingusagestrategies/BarehandsDig.java
@@ -1,5 +1,6 @@
 package projlab.iceisland.model.thingusagestrategies;
 
+import projlab.iceisland.Skeleton;
 import projlab.iceisland.model.Player;
 
 import static projlab.iceisland.model.Thing.*;
@@ -7,6 +8,8 @@ import static projlab.iceisland.model.Thing.*;
 public class BarehandsDig implements DiggingStrategy {
     @Override
     public void dig(Player who) {
+        Skeleton.called(this,"dig",who.toString());
         who.getCurrentField().dig(1);
+        Skeleton.returned(this,"dig",who.toString());
     }
 }
--- src/projlab/iceisland/model/thingusagestrategies/RopeRescue.java
+++ src/projlab/iceisland/model/thingusagestrategies/RopeRescue.java
@@ -8,8 +8,9 @@ import static projlab.iceisland.model.Thing.*;
 public class RopeRescue implements RescueStrategy {
     @Override
     public void rescuePlayer(Player who, int from) {
+        Skeleton.called(this,"rescuePlayer",who.toString()+", "+from.toString());
         who.getCurrentField().step(who.getCurrentField().getNeighbor(from).rescueSomeone());
         who.worked();
-
+        Skeleton.returned(this,"rescuePlayer",who.toString()+", "+from.toString());
     }
 }
